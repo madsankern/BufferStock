2b8d9e7b9aa02e8eb456bca078a001fffbf450d6
Your Name <you@example.com>
2022-12-07 10:35:54 +0100

    bug fix

--- last_period.py
+++ last_period.py
@@ -11,18 +11,25 @@ def solve(h,sol,par):
     v = sol.v[h]
 
     # Loop over states, no need to loop over fixed states
-    for i_z in nb.prange(par.Nz):
-        for i_a_lag in nb.prange(par.Na):
+    for i_alpha_l in nb.prange(par.Nalpha_l):
+        alpha_l = par.alpha_l_grid[i_alpha_l]
+        
+        for i_alpha_s in nb.prange(par.Nalpha_s):
+            alpha_s = par.alpha_s_grid[i_alpha_s]
+            
+            for i_z in nb.prange(par.Nz):
+                for i_a_lag in nb.prange(par.Na):
 
-            # Unpack states
-            z = par.z_grid[i_z]
-            a_lag = par.a_grid[i_a_lag]
+                    # Unpack states
+                    z = par.z_grid[i_z]
+                    a_lag = par.a_grid[i_a_lag]
 
-            # Define cash on hand
-            m = (1+par.r)*a_lag + par.w*z
+                    # Define cash on hand
+                    m = (1+par.r)*a_lag + par.w*z + alpha_l + h*alpha_s
 
-            # Optimal to consume everything. Choice does not depend on beta
-            c[:,:,:,i_z,i_a_lag] = m
-    
-            # value of choice
-            v[:,:,:,i_z,i_a_lag] = c[:,:,:,i_z,i_a_lag]**(1-par.sigma) / (1-par.sigma)
+                    # Optimal to consume everything. Choice does not depend on beta
+                    c[:,i_alpha_l,i_alpha_s,i_z,i_a_lag] = m
+
+                    # value of choice
+                    v[:,i_alpha_l,i_alpha_s,i_z,i_a_lag] = c[:,i_alpha_l,i_alpha_s,i_z,i_a_lag]**(1-par.sigma) / (1-par.sigma)
+
